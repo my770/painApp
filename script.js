b7249d83c4b6f47ebb93792aa4d2ed6126595591
Moh Younis <mohamed.younis@doctors.org.uk>
2024-09-28 01:31:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker.register("/sw.js").then(
+      function (registration) {
+        console.log(
+          "Service Worker registered with scope:",
+          registration.scope
+        );
+      },
+      function (error) {
+        console.log("Service Worker registration failed:", error);
+      }
+    );
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var coll = document.getElementsByClassName("collapsible");
+
+  function closeAllCollapsibles() {
+    for (var i = 0; i < coll.length; i++) {
+      coll[i].classList.remove("active");
+      var content = coll[i].nextElementSibling;
+      content.style.display = "none";
+    }
+  }
+
+  for (var i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+      var isActive = this.classList.contains("active");
+      closeAllCollapsibles();
+      if (!isActive) {
+        this.classList.add("active");
+        var content = this.nextElementSibling;
+        content.style.display = "block";
+      }
+    });
+  }
+});
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("header").classList.add("shifted");
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("header").classList.remove("shifted");
+}
+
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+
+  // Hide all tab content
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Remove active class from all tab links
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Hide the About section
+  document.getElementById("landingPage").style.display = "none";
+
+  // Show the selected tab content
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = "pain-management-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/script.js",
+  "/favicon.ico",
+  "/icon-192x192.png",
+  "/icon-512x512.png",
+];
+
+// Install a service worker
+self.addEventListener("install", function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Cache and return requests
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+// Update service worker
+self.addEventListener("activate", function (event) {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
