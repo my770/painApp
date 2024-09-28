c157bfead40eca0dd17c6467adfe80b85dce714a
Moh Younis <mohamed.younis@doctors.org.uk>
2024-09-28 02:25:55 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,50 +1,37 @@
 const CACHE_NAME = "pain-management-cache-v1";
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/styles.css",
-  "/script.js",
-  "/favicon.ico",
-  "/icon-192x192.png",
-  "/icon-512x512.png",
+  "/painApp/",
+  "/painApp/index.html",
+  "/painApp/styles.css",
+  "/painApp/script.js",
+  "/painApp/favicon.ico",
+  "/painApp/icon-192x192.png",
+  "/painApp/icon-512x512.png"
 ];
 
-// Install a service worker
-self.addEventListener("install", function (event) {
-  // Perform install steps
+self.addEventListener("install", function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log("Opened cache");
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log("Opened cache");
+        return cache.addAll(urlsToCache);
+      })
+      .catch(function(error) {
+        console.error("Failed to cache resources:", error);
+      })
   );
 });
 
-// Cache and return requests
-self.addEventListener("fetch", function (event) {
+// Fetch event to serve cached content
+self.addEventListener("fetch", function(event) {
   event.respondWith(
-    caches.match(event.request).then(function (response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
-  );
-});
-
-// Update service worker
-self.addEventListener("activate", function (event) {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        cacheNames.map(function (cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
   );
 });
